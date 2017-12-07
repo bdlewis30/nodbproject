61b52fd9d1c7b323918ff9b757f068cb8a15de0a
bdlewis30 <32585973+bdlewis30@users.noreply.github.com>
2017-12-06 22:41:24 -0700

    deleted header and footer and updated body

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './reset.css';
 import './App.css';
+import Body from './Components/Body/Body';
 
 class App extends Component {
   render() {
     return (
       <div>
-       
+        <Body />
       </div>
     );
   }
---
+++ src/Components/Body/Body.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './Body.css';
+
+export default class Body extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            text: 'Start Typing'
+        }
+    }
+    handleChange(event){
+        this.setState({
+            text: event.target.value
+        })
+    }
+
+    handleButton(){
+        alert(this.state.text)
+    }
+
+    render() {
+        return (
+            <div className="main">
+                <section className="header">Header</section>
+                {/* <input value={this.state.text} onChange={ (event) => {this.handleChange(event.target.value)}} />
+                <button>Submit</button> */}
+            </div>
+        )
+    }
+}
